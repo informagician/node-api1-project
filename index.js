272b8f5a0ac395c071e4b15fb76b56e58b69d638
Milo Rastgoo <hi@informagician.com>
2020-02-10 16:19:28 -0500

    List user by id

--- index.js
+++ index.js
@@ -29,11 +29,28 @@ server.post('/api/users', (req,res) => {
 server.get('/api/users', (req,res) => {
     db.find()
         .then(users => {
-            res.status(201).json(users)
+            res.status(200).json(users)
         })
         .catch(err => {
             console.log(err)
             res.status(500).json({errorMessage:"The users information could not be retrieved."})
+        })
+})
+
+// List User
+server.get('/api/users/:id', (req,res) => {
+    const {id} = req.params;
+
+    db.findById(id)
+        .then(user => {
+            if(!user) {
+                res.status(404).json({errorMessage:"The user with the specified ID does not exist."})
+            } else {
+                res.status(200).json(user)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({errorMessage: "The user information could not be retrieved."})
         })
 })
 const port = 5000;
